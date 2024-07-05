b486d1b0ca8442ce5b52a00a1f1d2d7ddff4a378
DongJoo Kim <djekko0701@gmail.com>
2024-07-05 15:53:35 +0900

    🎉 Begin Yakgwa!

---
+++ Yakgwa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Yakgwa
+//
+//  Created by Ekko on 7/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
