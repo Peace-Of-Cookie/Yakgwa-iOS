b4f96429349981f85b231335f36e9a43eb05fe08
DongJoo Kim <djekko0701@gmail.com>
2024-07-11 00:47:35 +0900

    💄 Ui: Design Splash ui #13

--- Scene/SplashScene/Sources/SplashScene/Presentation/SplashReactor.swift
+++ Scene/SplashScene/Sources/SplashScene/Presentation/SplashReactor.swift
@@ -6,3 +6,26 @@
 //
 
 import CoreKit
+import ReactorKit
+
+public final class SplashReactor: Reactor {
+    public enum Action {
+        case checkIfAuthenticated
+    }
+    
+    public enum Mutation {
+        case setAutenticated(Bool)
+    }
+    
+    public struct State {
+        var isAuthenticated: Bool?
+    }
+    
+    public var initialState = State()
+    
+    // TODO: Add Auth Service
+    
+    public init() {
+        
+    }
+}
