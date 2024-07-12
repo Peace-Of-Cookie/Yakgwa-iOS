69b03f590e1267e4699a5ad3d307ab0de3d8e701
DongJoo Kim <djekko0701@gmail.com>
2024-07-13 04:12:06 +0900

    ✨ Feat: Add APNs with FCM #23

---
+++ Scene/LoginScene/Sources/LoginScene/Presentation/LoginReactor.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ekko on 7/13/24.
+//
+
+import Foundation
--- Yakgwa/AppDelegate.swift
+++ Yakgwa/AppDelegate.swift
@@ -8,13 +8,31 @@
 import UIKit
 
 import CoreKit
+
 import KakaoSDKCommon
+import FirebaseCore
+import FirebaseMessaging
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Register Fonts
         Font.registerYakgwaFont()
+        
+        // Kakao SDK
         KakaoSDK.initSDK(appKey: "6125fa6ae1efc29d385873c2b891e24e")
+        
+        // Firebase
+        FirebaseApp.configure()
+        
+        let current = UNUserNotificationCenter.current()
+        current.delegate = self
+        current.requestAuthorization(options: [.alert, .sound, .badge]) { _, _ in }
+        application.registerForRemoteNotifications()
+        
+        Messaging.messaging().delegate = self
+        
         return true
     }
 
@@ -31,7 +49,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate, MessagingDelegate {
+    // 백그라운드에서 푸시 알림을 탭했을 때 실행
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("APNS token: \(deviceToken)")
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    // Foreground
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
+        return [.badge, .sound, .list]
+    }
+    
+    // 파이어베이스 MessagingDelegate 설정
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("Firebase registration token: \(String(describing: fcmToken))")
+    }
+}
