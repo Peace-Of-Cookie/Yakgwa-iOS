fdb06a81200da638cb08111d2adc6a44cee1583e
DongJoo Kim <djekko0701@gmail.com>
2024-09-19 01:32:22 +0900

    💄 UI: Add MyPage UI Components (#88)

--- Scene/MyPageScene/Sources/MyPageScene/Presentation/MyPageViewController.swift
+++ Scene/MyPageScene/Sources/MyPageScene/Presentation/MyPageViewController.swift
@@ -13,6 +13,66 @@ public final class MyPageViewController: UIViewController {
     // MARK: - Properties
     
     // MARK: - UI Components
+    private lazy var sceneTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "마이페이지"
+        label.font = .sb18
+        label.textColor = .neutralBlack
+        return label
+    }()
+    
+    private lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        return view
+    }()
+    
+    private lazy var contentView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private lazy var myPageStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 8
+        return stack
+    }()
+    
+    private lazy var profileContainer: UIView = {
+        let view = UIView()
+        view.backgroundColor = .neutralWhite
+        view.layer.cornerRadius = 25
+        return view
+    }()
+    
+    private lazy var profileImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "default_profile_image", in: .module, with: nil)
+        return view
+    }()
+    
+    private lazy var profileLabelStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 6
+        return stack
+    }()
+    
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "{이름}님"
+        label.font = .sb16
+        label.textColor = .neutralBlack
+        return label
+    }()
+    
+    private lazy var welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "환영합니다"
+        label.font = .m16
+        label.textColor = .neutralBlack
+        return label
+    }()
     
     // MARK: - Initializers
     public init() {
@@ -32,6 +92,15 @@ public final class MyPageViewController: UIViewController {
     
     private func setUI() {
         self.view.backgroundColor = .neutral200
+        
+        self.view.addSubview(sceneTitleLabel)
+        sceneTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(16)
+            $0.leading.equalToSuperview().offset(16)
+        }
+        
+        self.view.addSubview(profileContainer)
+        
     }
 }
 
